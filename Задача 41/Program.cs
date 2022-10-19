f7084f9b6eaa09c06a41374b9161ee8168678682
Ygor <wanerosu8@gmail.com .com>
2022-10-19 19:04:47 +0300

    first commit

---
+++ Задача 41/Program.cs
@@ -0,0 +1,123 @@
+﻿
+
+/* 
+1 Пользователь вводит с клавиатуры M чисел. 
+2 Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+
+
+
+
+
+int CountGreaterZero(int[] InputInternalArray)
+{
+    int Result = 0;
+    for (int i = 0; i < InputInternalArray.Length; i++)
+    {
+        if (InputInternalArray[i] > 0)
+        {
+            Result++;
+        }
+    }
+    return Result;
+}
+
+void PrintArray(int[] InputInternalArray)
+{
+    Console.Write("Массив = ");
+    Console.Write("[");
+    for (int i = 0; i < InputInternalArray.Length; i++)
+    {
+        Console.Write(InputInternalArray[i]);
+        if (i < InputInternalArray.Length - 1) { Console.Write(", "); }
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+int[] MadeAndFillArray(int InpArrayLength)
+{
+    int[] InternalArray = new int[InpArrayLength];
+    for (int i = 0; i < InternalArray.Length; i++)
+    {
+        Console.Write($"Введите число с индексом №{i} = ");
+        InternalArray[i] = int.Parse(Console.ReadLine());
+    }
+    return InternalArray;
+}
+
+
+
+
+
+Console.Write("Введите количество чисел в массиве: ");
+int NomberOfNombersInArray = int.Parse(Console.ReadLine());
+
+
+int[] AArray = MadeAndFillArray(NomberOfNombersInArray);
+
+PrintArray(AArray);
+
+Console.Write("Числа больше 0 в массиве: ");
+Console.Write(CountGreaterZero(AArray));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* int[] InputNStirngArray()
+{
+    Console.WriteLine();
+
+    bool inStopCycle = false;
+    int[] InternalArray = null;
+
+    int TempInt = 0;
+    int Iteration = 0;
+
+    Console.WriteLine("Начата процедура ввода массива. Введите 'stop' (буква) чтобы прекратить ввод");
+
+    while (inStopCycle == false)
+    {
+        Console.Write($"Введите число с индексом №{Iteration} = ");
+
+        var InpotNomber = Console.ReadLine();
+
+
+
+        if (InpotNomber != "stop")
+        {
+            //InternalArray.Resize(ref InternalArray[], InternalArray.Length + 1);
+            //InternalArray[InternalArray.Length] = int.Parse(InpotNomber);
+            // InternalArray.Add(int.Parse(InpotNomber));
+            InternalArray[InternalArray.Length] = int.Parse(InpotNomber);
+        }
+        else
+        {
+            Console.WriteLine("Процедура ввода массива Закончена.");
+            inStopCycle = true;
+        }
+
+        //Console.WriteLine("цикл ввода закончен ------------------------");
+        Iteration++;
+
+    }
+    Console.WriteLine();
+    return InternalArray;
+} */
+
---
+++ Задача 43/Program.cs
@@ -0,0 +1,98 @@
+﻿
+
+
+
+/*
+Напишите программу,
+которая найдёт точку пересечения двух прямых,
+заданных уравнениями
+y = k1 * x + b1,
+y = k2 * x + b2;
+
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+
+Console.Write("Введите константу b1: ");
+int b1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите константу k1: ");
+int k1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите константу b2: ");
+int b2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите константу k2: ");
+int k2 = int.Parse(Console.ReadLine());
+
+
+
+
+
+
+
+double SolveSmalUravnenie(int b, int k, double x)
+{
+    double y = k * x + b;
+    return y;
+}
+
+
+
+
+double[] SolveBigUravnenie(int b1, int k1, int b2, int k2)
+{
+    double[] CoordinatesXY = new double[2];
+    bool StopCycle = false;
+    double x = -100.00;
+
+    double RX = 0;
+    double RY = 0;
+
+    while (StopCycle != true)
+    {
+        RX = SolveSmalUravnenie(b1, k1, x);
+        RY = SolveSmalUravnenie(b2, k2, x);
+
+        RX = Math.Round(RX, 2);
+        RY = Math.Round(RY, 2);
+
+
+        if (RX == RY)
+        {
+            Console.WriteLine("Процедура цикла Закончена.");
+            StopCycle = true;
+        }
+
+        if (x > 100.00)
+        {
+            Console.WriteLine("точка пересечения очень далеко");
+            StopCycle = true;
+        }
+
+        Console.WriteLine($"X={Math.Round(x, 2)} A={RX}  B={RY}");
+
+        x += 0.01;
+    }
+
+    CoordinatesXY[0] = RX;
+    CoordinatesXY[1] = RY;
+
+    return CoordinatesXY;
+}
+
+
+
+
+
+
+Console.WriteLine("----------------------");
+
+double[] AArrayXY = new double[2];
+AArrayXY = SolveBigUravnenie(b1, k1, b2, k2);
+
+Console.WriteLine("----------------------");
+
+Console.WriteLine($"Координаты пересечения: ({AArrayXY[0]};{AArrayXY[1]})");
